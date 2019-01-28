0f9a18fc6b75246a9abd761d3425f33a3a7a4e3c
Jimjule <roadknightj@gmail.com>
2019-01-28 12:14:15 +0000

    Added comments about the structure of the directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First, we print the list of students
 puts "The students of Villains Academy"
 puts "----------"
 puts "Hannibal Lector"
@@ -6,6 +7,8 @@ puts "Michael Corleone"
 puts "Terminator"
 puts "The Joker"
 puts "Norman Bates"
+# Then, we print the number of students
 print "Overall we have "
+# Print is used to avoid new lines
 print 6
 print " great students"
