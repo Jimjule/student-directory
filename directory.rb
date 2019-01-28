afcdf0c8563819edca5278d1f27c4742545507ff
Jimjule <roadknightj@gmail.com>
2019-01-28 12:49:05 +0000

    Refactored using methods

--- directory.rb
+++ directory.rb
@@ -1,12 +1,28 @@
-student_count = 6
-# First, we print the list of students
-puts "The students of Villains Academy"
-puts "----------"
-puts "Hannibal Lector"
-puts "Darth Vader"
-puts "Michael Corleone"
-puts "Terminator"
-puts "The Joker"
-puts "Norman Bates"
-# Then, we print the number of students
-puts "Overall we have #{student_count} great students"
+# First, we make an array of students
+students = [
+  "Hannibal Lector",
+  "Darth Vader",
+  "Michael Corleone",
+  "Terminator",
+  "The Joker",
+  "Norman Bates"
+]
+# And then print them
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
