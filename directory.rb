15b8f792c1c2eef9f3dda0b8a3ab68a62249b056
Jimjule <roadknightj@gmail.com>
2019-01-28 12:03:06 +0000

    Added directory.rb, a list of students

---
+++ directory.rb
@@ -0,0 +1,8 @@
+puts "The students of Villains Academy"
+puts "----------"
+puts "Hannibal Lector"
+puts "Darth Vader"
+puts "Michael Corleone"
+puts "Terminator"
+puts "The Joker"
+puts "Norman Bates"
