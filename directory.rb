761daaf6ff9598308a55a3af55c0368d25031ca5
Jimjule <roadknightj@gmail.com>
2019-01-30 16:56:00 +0000

    Refactored the code with methods and accessible variables

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just leave the answers blank"
-  students = []
+  @students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+  @students
 end
 
 students = input_students
@@ -19,20 +19,48 @@ def print_header
   puts "----------"
 end
 
-def print(students)
+def print_student_list
   i = 0
-  while i < students.length
-    puts "#{students[i][:name]} #{students[i][:cohort]} cohort"
+  while i < @students.length
+    puts "#{@students[i][:name]} #{@students[i][:cohort]} cohort"
     i += 1
   end
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.count} great students"
+def print_footer
+  puts "Overall we have #{@students.count} great students"
 end
 
-if students.length > 0
-  print_header
-  print(students)
-  print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
 end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process
+  case selection = gets.chomp
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process
+  end
+end
+
+interactive_menu
